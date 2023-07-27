50122948b8fa8188187a2d5c138ffa6ff8384f5e
Mohamed Krimi <contact@medkrimi.net>
2023-07-27 11:23:04 -0400

    Custom Cracker Barrel config+logo demo

--- src/components/branding/HeaderLogo.tsx
+++ src/components/branding/HeaderLogo.tsx
@@ -1,11 +1,12 @@
 import { Image, useColorMode } from "@chakra-ui/react"
+
 import { Link } from "../navigation/Link"
 
 const HeaderLogo = () => {
   const { colorMode } = useColorMode()
   return (
     <Link href="/">
-      <Image w={165} src={colorMode === "dark" ? "/vector/my-commerce--dark.svg" : "/vector/my-commerce--default.svg"} alt="MyCommerce Marketplace" />
+      <Image w={165} src={colorMode === "dark" ? "/vector/cracker-barrel--dark.svg" : "/vector/cracker-barrel--default.svg"} alt="Cracker Barrel" />
     </Link>
   )
 }
