ed1f6d5a36ec7575262fa9d4d45ac255e89d262f
Mohamed Krimi <contact@medkrimi.net>
2023-11-15 21:21:10 -0500

    Fixes

--- src/components/branding/HeaderLogo.tsx
+++ src/components/branding/HeaderLogo.tsx
@@ -1,9 +1,9 @@
-import { Image, useColorMode } from "@chakra-ui/react"
+import {Image, useColorMode} from "@chakra-ui/react"
 
-import { Link } from "../navigation/Link"
+import {Link} from "../navigation/Link"
 
 const HeaderLogo = () => {
-  const { colorMode } = useColorMode()
+  const {colorMode} = useColorMode()
   return (
     <Link href="/">
       <Image w={165} src={colorMode === "dark" ? "/vector/isc2--dark.svg" : "/vector/isc2--default.svg"} alt="ISC2" />
